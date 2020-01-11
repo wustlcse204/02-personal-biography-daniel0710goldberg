86e22b180bee6f37d6f09c8b294631f77b41015c
Daniel Goldberg <d.goldberg@wustl.edu>
2020-01-11 15:42:26 -0500

    yes

--- javascript.js
+++ javascript.js
@@ -78,7 +78,7 @@ function moveB(ball){
 
 function moveS(ball){
 
-  setInterval(function(){moveSLeft(ball)}, 30);
+  setInterval(function(){moveSLeft(ball)}, 35);
 
 }
 
@@ -103,7 +103,7 @@ function moveSLeft(ball){
 
   var y = Math.floor((Math.random() * 15) + 5);
   var z = Math.floor((Math.random() * 2));
-  var up = Math.floor((Math.random() * 30) + 1);
+  var up = Math.floor((Math.random() * 40) + 1);
 
   ball.style.right =
     parseInt(ball.style.right) + y + 'px';
